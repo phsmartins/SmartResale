a14936bb4edbcad1922c6388f605d11895892a75
phsmartins <phsmartins2004@gmail.com>
2024-08-10 18:06:54 -0400

    Try Catch para se conectar ao banco de dados

--- config/dependencies.php
+++ config/dependencies.php
@@ -4,7 +4,18 @@ $builder = new \DI\ContainerBuilder();
 $builder->addDefinitions([
     PDO::class => function (): PDO {
         $dbPath = __DIR__ . '/../database.sqlite';
-        return new PDO("sqlite:{$dbPath}");
+
+        try {
+            $pdo = new PDO("sqlite:{$dbPath}");
+            $pdo->exec("PRAGMA foreign_keys = ON;");
+
+            return $pdo;
+        } catch (PDOException $e) {
+            echo "<h1>Erro ao conectar ao banco de dados</h1>";
+            exit();
+
+            // throw new RuntimeException('Erro ao conectar ao banco de dados: ' . $e->getMessage());
+        }
     },
     \League\Plates\Engine::class => function () {
         $templatePath = __DIR__ . '/../views';
