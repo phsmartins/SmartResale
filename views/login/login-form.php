5ae5ddd7eb040940fd252d823b1167e9042dcb08
phsmartins <phsmartins2004@gmail.com>
2024-08-12 15:03:47 -0400

    Primeiras estilizações da página de login

--- views/login/login-form.php
+++ views/login/login-form.php
@@ -1,3 +1,26 @@
-<?php
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-echo "Hello World - Login";
+    <link rel="stylesheet" href="/style/main.css">
+    <link rel="stylesheet" href="/style/login.css">
+
+    <title>SmartResale | Entre ou Cadastre-se</title>
+</head>
+<body>
+
+    <main>
+        <div class="welcome__box">
+            <img src="/images/logo/logo-branca-SmartResale.png" alt="Logo da SmartResale" />
+
+            <h1>Controle suas vendas do começo ao fim</h1>
+        </div>
+
+        <div class="form__box">
+
+        </div>
+    </main>
+
+</body>
