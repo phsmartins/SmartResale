e1ff7fc1fe85f09408aa654c2ea02936c48f6fde
phsmartins <phsmartins2004@gmail.com>
2024-08-12 16:58:12 -0400

    Formulário de login

--- views/login/login-form.php
+++ views/login/login-form.php
@@ -13,13 +13,27 @@
 
     <main>
         <div class="welcome__box">
-            <img src="/images/logo/logo-branca-SmartResale.png" alt="Logo da SmartResale" />
+            <img class="welcome__logo" src="/images/logo/logo-branca-SmartResale.png" alt="Logo da SmartResale" />
 
+            <img class="welcome__image-login" src="/images/image-login.png" alt="Imagem para representar gestão em relação ao sistema" />
             <h1>Controle suas vendas do começo ao fim</h1>
         </div>
 
         <div class="form__box">
+            <form method="post">
+                <h2>Olá, bem-vindo!</h2>
+                <p>Digite as credênciais para acessar o sistema</p>
 
+                <label for="email">E-mail</label>
+                <input type="email" name="email" id="email" />
+
+                <label for="password">Senha</label>
+                <input type="password" name="password" id="password">
+
+                <button type="submit">Acessar</button>
+
+                <a>Não tem conta? Cadastre-se</a>
+            </form>
         </div>
     </main>
 
