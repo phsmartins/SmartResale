38e0bc825c9aa22ea5c29d47128e1ffcf346ad4f
phsmartins <phsmartins2004@gmail.com>
2024-08-12 15:03:01 -0400

    Arquivo de estilo padrão adicionado

--- views/error-404.php
+++ views/error-404.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <link rel="stylesheet" href="/style/main.css">
     <link rel="stylesheet" href="/style/error404.css">
 
     <title>SmartResale | Página não encontrada</title>
