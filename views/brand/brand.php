dfdace00ae1e9d469c2eb36397e949def604d56a
phsmartins <phsmartins2004@gmail.com>
2024-11-14 16:48:38 -0400

    Exemplo do uso de gráfico

--- views/brand/brand.php
+++ views/brand/brand.php
@@ -4,6 +4,8 @@ $this->layout('layout');
 /** @var \Smart\Resale\Entity\Brand $brand */
 ?>
 
+<link rel="stylesheet" href="/style/brand.css">
+
 <main>
     <div class="headerViewSingleItem">
         <h1><?= $brand->getBrandName(); ?></h1>
@@ -13,7 +15,91 @@ $this->layout('layout');
 
     <hr>
 
-    <h1>(Tela em desenvolvimento)</h1>
+    <div class="main_info">
+        <canvas id="myChart"></canvas>
+
+        <div class="verticalLine"></div>
+
+        <div class="general_info">
+            <h2>Informações gerais</h2>
+
+            <div class="single_info">
+                <p>Faturamento:</p>
+                <p class="single_info__data"><?= $brand->getInvoicing(); ?></p>
+            </div>
+
+            <div class="single_info">
+                <p>Lucro:</p>
+                <p class="single_info__data"><?= $brand->getProfit(); ?></p>
+            </div>
+
+            <div class="single_info">
+                <p>Produtos vendidos:</p>
+                <p class="single_info__data"><?= $brand->getQuantityOfProductsSold(); ?></p>
+            </div>
+
+            <div class="single_info">
+                <p>Produtos cadastrados:</p>
+                <p class="single_info__data"><?= $brand->getNumberRegisteredProducts(); ?></p>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+    <script>
+        const ctx = document.getElementById('myChart');
+
+        new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+                datasets: [{
+                    label: 'Faturamento',
+                    data: [
+                        6000,
+                        4000,
+                        8000,
+                        3000,
+                        1000,
+                        2000,
+                        6000,
+                        4000,
+                        2000,
+                        1600,
+                        5000,
+                        3000,
+                    ],
+                    borderWidth: 2
+                }]
+            },
+            options: {
+                plugins: {
+                    title: {
+                        display: true,
+                        text: 'Faturamento - 2024',
+                        font: {
+                            size: 24,
+                            weight: 'bold'
+                        }
+                    }
+                },
+                scales: {
+                    y: {
+                        beginAtZero: true,
+                        ticks: {
+                            callback: function(value) {
+                                return new Intl.NumberFormat('pt-BR', {
+                                    style: 'currency',
+                                    currency: 'BRL'
+                                }).format(value);
+                            }
+                        }
+                    }
+                }
+            }
+        });
+    </script>
 
     <div style="display: none" id="editModal" class="animationModal">
         <div class="modalTitle">
